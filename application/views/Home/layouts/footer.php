63eef95e7602329838a16a830c23280f40ffb5b2
thulee <lethu210398@gmail.com>
2019-03-18 22:43:42 +0700

    update

---
+++ application/views/Home/layouts/footer.php
@@ -0,0 +1,8 @@
+<script src="public/assets/themes/coaching/js/bootstrap.min.js"></script>
+
+<script src="public/assets/themes/coaching/js/wow.min.js"></script>
+<script>
+    new WOW().init();
+</script>
+
+
